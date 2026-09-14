c361ab3881943e82360cdec1e17a6972c6228828
pgordon-moric <pgordon@moric.org>
2026-09-14 16:10:18 -0400

    minor edits

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,7 +9,10 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    const total = 0;
+    cart.forEach((item) => {
+        
+    });
   };
 
   const handleContinueShopping = (e) => {

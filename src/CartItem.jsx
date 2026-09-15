c3c182486cbeaa9e9fa39c63f6aede56fc04946b
pgordon-moric <pgordon@moric.org>
2026-09-15 13:01:00 -0400

    wrote logic for CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,30 +9,41 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
-    const total = 0;
+    let total = 0;
     cart.forEach((item) => {
-        
+        total += item.quantity * parseFloat(item.cost.substring(1));
     });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+       alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity+1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1){
+   dispatch(updateQuantity({ name: item.name, quantity: item.quantity-1}));
+    } else {
+        dispatch(removeItem({ name: item.name}));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({ name: item.name }));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let totalCost = item.quantity * parseFloat(item.cost.substring(1));
+    return totalCost;
   };
 
   return (
